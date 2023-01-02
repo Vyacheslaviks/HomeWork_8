1ea00fc5af3318aa8bd3fbc16807db3f4283a99f
Slava <slavanew98@mail.ru>
2023-01-02 15:50:07 +0300

    Initialize repositore with 2 exercise

---
+++ MyMethods/Class1.cs
@@ -0,0 +1,18 @@
+﻿namespace MyMethods;
+
+public static class Sl
+{
+    public static int InputInt()
+    {
+        bool isParse = int.TryParse(Console.ReadLine(), out int number);
+
+        if (isParse)
+        {
+            return number;
+        }
+        else
+        {
+            throw new ArgumentException("Введено некорректное значение!");
+        }
+    }
+}
